9354433b4dbb79fe4ce0d19aa20773905c3bccdc
Hushperry <timileyintaiwo269@gmail.com>
2026-08-21 09:49:57 +0100

    Add click event listener to buttons for alerts
    Make buttons interactive

--- script.js
+++ script.js
@@ -111,3 +111,10 @@ footer {
         margin: 6px;
     }
 }
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        alert("Thank you for choosing Glow Beauty & Spa! We will contact you soon.");
+    });
+});
